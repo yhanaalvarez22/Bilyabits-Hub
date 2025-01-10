8bc241ce37f394fbdd7da5c30abbc2038922eb90
Carl John B. Villavito <carljohnvillavito2023@gmail.com>
2025-01-10 17:19:03 +0800

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -1,1 +1,17 @@
+const fs = require('fs');
+const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 
+module.exports = {
+    name: 'help',
+    description: 'List all available commands',
+    execute(api, event) {
+        const commandFiles = fs.readdirSync('./cmds').filter(file => file.endsWith('.js'));
+        let message = 'C ° O ° M ° M ° A ° N ° D ° S\n\n';
+
+        commandFiles.forEach(file => {
+            const command = require(`./${file}`);
+            message += `[~] ${command.name}\n     [°°°] ${command.description}\n\n`;
+        });
+        api.sendMessage(message, event.threadID);
+    }
+};
